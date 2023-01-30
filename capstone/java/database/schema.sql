44646520ae9809dbd520b921b49fafb14d1da7f7
Laura Nazzaro <peaceandcloud@gmail.com>
2023-01-30 14:58:44 -0800

    sql: updated users, added pets, volunteer, and adopter tables

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,13 +1,47 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS volunteers, users, adopter, pets;
+
+CREATE TABLE volunteers  (
+application_id SERIAL,
+name varchar(100) NOT NULL,
+email varchar(50) NOT NULL,
+over_18 boolean NOT NULL,
+role varchar(10) NOT NULL,
+status varchar(10) NOT NULL,
+CONSTRAINT PK_volunteers PRIMARY KEY (application_id)
+);
 
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+	application_id int NOT NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id),
+	CONSTRAINT FK_user FOREIGN KEY (application_id) REFERENCES volunteers (application_id)
+);
+
+CREATE TABLE adopter (
+adopter_id SERIAL,
+adopter_name varchar(100) NOT NULL,
+CONSTRAINT PK_adopter PRIMARY KEY (adopter_id)
+);
+
+CREATE TABLE pets (
+pet_id SERIAL,
+pet_image varchar(500),
+name varchar(70) NOT NULL,
+type varchar(70) NOT NULL,
+age int NOT NULL,
+gender varchar(70) NOT NULL,
+weight int NOT NULL,
+breed varchar(70) NOT NULL,
+description varchar(1000) NOT NULL,
+adopted boolean NOT NULL,
+adopter_id varchar(100),
+CONSTRAINT PK_pets PRIMARY KEY (pet_id),
+CONSTRAINT FK_pets FOREIGN KEY (adopter_id) REFERENCES adopter (adopter_id)
 );
 
 COMMIT TRANSACTION;
