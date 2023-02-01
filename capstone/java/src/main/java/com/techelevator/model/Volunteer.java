1b9b36599a1c1287767bfd55e3166ef9dbe88961
Laura Nazzaro <peaceandcloud@gmail.com>
2023-02-01 11:45:25 -0800

    updating volunteer form and database 1

--- capstone/java/src/main/java/com/techelevator/model/Volunteer.java
+++ capstone/java/src/main/java/com/techelevator/model/Volunteer.java
@@ -3,10 +3,14 @@ package com.techelevator.model;
 public class Volunteer {
 
     private int applicationId;
-    private String volunteerName;
+    private String volunteerFirstName;
+    private String volunteerLastName;
     private String email;
     private boolean over18;
-    private String volunteerRole = "ROLE_USER";
+    private boolean veterinary;
+    private boolean cleaning;
+    private boolean dataEntry;
+    private boolean photography;
     private String status = "Pending";
 
     public int getApplicationId() {
@@ -17,12 +21,20 @@ public class Volunteer {
         this.applicationId = applicationId;
     }
 
-    public String getVolunteerName() {
-        return volunteerName;
+    public String getVolunteerFirstName() {
+        return volunteerFirstName;
     }
 
-    public void setVolunteerName(String volunteerName) {
-        this.volunteerName = volunteerName;
+    public void setVolunteerFirstName(String volunteerFirstName) {
+        this.volunteerFirstName = volunteerFirstName;
+    }
+
+    public String getVolunteerLastName() {
+        return volunteerLastName;
+    }
+
+    public void setVolunteerLastName(String volunteerLastName) {
+        this.volunteerLastName = volunteerLastName;
     }
 
     public String getEmail() {
@@ -41,19 +53,43 @@ public class Volunteer {
         this.over18 = over18;
     }
 
-    public String getVolunteerRole() {
-        return volunteerRole;
-    }
-
-    public void setVolunteerRole(String volunteerRole) {
-        this.volunteerRole = volunteerRole;
-    }
-
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public boolean isVeterinary() {
+        return veterinary;
+    }
+
+    public void setVeterinary(boolean veterinary) {
+        this.veterinary = veterinary;
+    }
+
+    public boolean isCleaning() {
+        return cleaning;
+    }
+
+    public void setCleaning(boolean cleaning) {
+        this.cleaning = cleaning;
+    }
+
+    public boolean isDataEntry() {
+        return dataEntry;
+    }
+
+    public void setDataEntry(boolean dataEntry) {
+        this.dataEntry = dataEntry;
+    }
+
+    public boolean isPhotography() {
+        return photography;
+    }
+
+    public void setPhotography(boolean photography) {
+        this.photography = photography;
     }
 }
