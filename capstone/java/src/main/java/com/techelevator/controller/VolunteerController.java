5a9f48114841fca8d2b9374f9639028942c0dc67
Collin OConner <croconner522@gmail.com>
2023-02-03 23:07:41 -0500

    Emails when applied user is approved or denied

--- capstone/java/src/main/java/com/techelevator/controller/VolunteerController.java
+++ capstone/java/src/main/java/com/techelevator/controller/VolunteerController.java
@@ -2,16 +2,16 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.UserDao;
 import com.techelevator.dao.VolunteerDao;
-import com.techelevator.model.User;
+import com.techelevator.model.GMailer;
 import com.techelevator.model.Volunteer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Locale;
 
 @RequestMapping(path = "/volunteer")
 @RestController
@@ -46,13 +46,22 @@ public class VolunteerController {
     // Where to get hasRole name
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping(path = "/{id}")
-    public void approveDenyApplication(@PathVariable int id, @RequestBody Volunteer application){
+    public void approveDenyApplication(@PathVariable int id, @RequestBody Volunteer application) throws Exception {
+        Volunteer volunteer = volunteerDao.getVolunteerById(id);
+        String password = "" + (int) (Math.random() * 100);
+        String username = volunteer.getVolunteerFirstName() + "." + volunteer.getVolunteerLastName();
+        String email = volunteer.getEmail();
+        GMailer sendEmail = new GMailer();
             if (application.getStatus().equals("Approved")) {
                 volunteerDao.updateStatus(id, "Approved");
-                Volunteer volunteer = volunteerDao.getVolunteerById(id);
-                userDao.create(volunteer.getVolunteerFirstName() + "." + volunteer.getVolunteerLastName(), "" + (int) (Math.random() * 100), "ROLE_USER");
+                userDao.create(username, password, "ROLE_USER");
+                sendEmail.sendMail(email, "Welcome to the team!",
+                        "Dear " + volunteer.getVolunteerFirstName() + " you have been approved to start working on our team at the Critter Cottage!" +
+                                "\n\n\n Here are you're login credentials:\nUsername: " + username + "\nPassword: " + password);
             } else {
                 volunteerDao.updateStatus(id, "Denied");
+                sendEmail.sendMail(email, "Critter Cottage information",
+                        "Sorry " + volunteer.getVolunteerFirstName() + " Critter Cottage doesn't want you");
             }
     }
 
